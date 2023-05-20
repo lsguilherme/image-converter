5730c5d7ac2f575df69d0d8117f94bd73f72ecf4
lsguilherme <lacerdagui42@gmail.com>
2023-05-19 23:15:43 -0300

    ajuste na rota de imagens

--- api/routes/imagens.js
+++ api/routes/imagens.js
@@ -1,27 +1,26 @@
-import express from 'express'
-import multer from 'multer'
-import zmq from 'zeromq'
-const router = express.Router()
-const upload = multer()
+import express from "express";
+import multer from "multer";
+import zmq from "zeromq";
+const router = express.Router();
+const upload = multer();
 
-router.post('/', upload.single('imagem'), async (req, res) => {
-    // console.log(req.file)
+router.post("/", upload.single("imagem"), async (req, res) => {
+  //   console.log(req.file);
 
-    const sock = new zmq.Request
+  const sock = new zmq.Request();
 
-    sock.connect("tcp://127.0.0.1:3001")
+  sock.connect("tcp://127.0.0.1:3001");
 
-    await sock.send(req.file.buffer)
-    const [result] = await sock.receive()
+  await sock.send(req.file.buffer);
+  const [result] = await sock.receive();
 
-    console.log(result)
-    
-    // res.set({
-    //     "Content-Type": req.file.mimetype,
-    //     "Content-Disposition": `attachment; filename=${req.file.originalname}`
-    // });
-    res.end(result);
+  console.log(result);
+
+  // res.set({
+  //     "Content-Type": req.file.mimetype,
+  //     "Content-Disposition": `attachment; filename=${req.file.originalname}`
+  // });
+  res.end(result);
 });
-
 
 export default router;
