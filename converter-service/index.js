e35b16aa39e84c04a555bd86e39cf657cf1858e4
lsguilherme <lacerdagui42@gmail.com>
2023-05-19 23:16:33 -0300

    Aceitando o buffer de imagem e retornando para api

--- converter-service/index.js
+++ converter-service/index.js
@@ -1,20 +1,17 @@
 import sharp from "sharp";
 import zmq from "zeromq";
 
-const sock = new zmq.Reply
-await sock.bind("tcp://127.0.0.1:3001")
-
-function converterJpgParaPng(nomeArquivoEntrada, nomeArquivoSaida) {
-  sharp(nomeArquivoEntrada)
-    .png()
-    .toFile(nomeArquivoSaida)
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-}
+const sock = new zmq.Reply();
+await sock.bind("tcp://127.0.0.1:3001");
 
 for await (const [msg] of sock) {
-  console.log(msg)
-  converterJpgParaPng("assets/rocklee.webp", "rocklee.png");
-  await sock.send(msg)
+  sharp(msg)
+    .jpeg()
+    .toBuffer()
+    .then((data) => {
+      sock.send(data);
+    })
+    .catch((err) => {
+      sock.send(err);
+    });
 }
-
