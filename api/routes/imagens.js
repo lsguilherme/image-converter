a6a67896a520f9f6bb13fa0937a4a5cc5cabef75
lsguilherme <lacerdagui42@gmail.com>
2023-05-20 00:42:48 -0300

    ajuste console

--- api/routes/imagens.js
+++ api/routes/imagens.js
@@ -1,25 +1,22 @@
-import express from 'express'
-import multer from 'multer'
-import zmq from 'zeromq'
-const router = express.Router()
-const upload = multer()
+import express from "express";
+import multer from "multer";
+import zmq from "zeromq";
+const router = express.Router();
+const upload = multer();
 
-router.post('/', upload.single('imagem'), async (req, res) => {
-    const sock = new zmq.Request
+router.post("/", upload.single("imagem"), async (req, res) => {
+  const sock = new zmq.Request();
 
-    sock.connect("tcp://127.0.0.1:3001")
+  sock.connect("tcp://127.0.0.1:3001");
 
-    await sock.send(req.file.buffer)
-    const [result] = await sock.receive()
+  await sock.send(req.file.buffer);
+  const [result] = await sock.receive();
 
-    console.log(result)
-
-    res.set({
-        "Content-Type": 'image/jpeg',
-        "Content-Disposition": `attachment; filename=${req.file.originalname}`
-    });
-    res.end(result);
+  res.set({
+    "Content-Type": "image/jpeg",
+    "Content-Disposition": `attachment; filename=${req.file.originalname}`,
+  });
+  res.end(result);
 });
-
 
 export default router;
